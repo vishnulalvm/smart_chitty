9f0a4d1a8fb6ebb4f2251978710a3526fd48fc8c
vishnulalvm <vishnulalvm007@gmail.com>
2024-03-29 22:14:09 +0530

    fix exeption

--- lib/services/db functions/transctiondata_function.dart
+++ lib/services/db functions/transctiondata_function.dart
@@ -9,28 +9,18 @@ Future<void> fetchMemberDatas() async {
   data = transactionDb;
 data.sort((b, a) {
   try {
-    // Split the month string into month and year components
     final aMonthComponents = a.month.split('-');
     final bMonthComponents = b.month.split('-');
-
-    // Parse the month components as integers
     final aMonthNum = int.parse(aMonthComponents[0]);
     final bMonthNum = int.parse(bMonthComponents[0]);
-
-    // Parse the year components as integers
     final aYear = int.parse(aMonthComponents[1]);
     final bYear = int.parse(bMonthComponents[1]);
-
-    // Compare the years first
     if (aYear != bYear) {
       return aYear.compareTo(bYear);
     }
-
-    // If the years are the same, compare the months
     return aMonthNum.compareTo(bMonthNum);
   } catch (e) {
-    // Handle invalid format by returning a default value or taking appropriate action
-    print('Invalid month format: ${a.month}');
+   
     return 0;
   }
 });
